4d8709cc3b2d3f2af65cd50843a7e8a034ee8449
wquynguy <wquyenle@gmail.com>
2024-01-30 10:18:20 -0600

    iadded and imported enrollment.js contents component to home.js component

--- src/components/Enrollment.js
+++ src/components/Enrollment.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { Button, Modal } from 'antd';
 
-const Enrollment = () => {
-  return (
-    <div>Enrollment</div>
-  );
+class Enrollment extends React.Component {
+    state = { visible: false };
+
+    showModal = () => {
+        this.setState({
+            visible: true,
+        });
+    };
+
+    handleCancel = e => {
+        console.log(e);
+        this.setState({
+            visible: false,
+        });
+    };
+
+    render () {
+        return (
+            <div className="block worksBlock">
+                <div className="container-fluid">
+                    <div className="titleHolder">
+                        <h2> Enrollment Process </h2>
+                        <p> Enroll in Polyglot Pages by completing our online application, scheduling a tour of our facility, and meeting with our admissions team to discuss your child's educational needs and language learning goals  </p>
+                    </div>
+                    <div className="contentHolder">
+                    <Button type="primary" onClick={this.showModal}> 
+                    <i class="fas fa-play"></i>
+                    </Button>
+                    </div>
+                    <Modal 
+                        title="Basic Modal" 
+                        visible={this.state.visible}
+                        onCancel={this.handleCancel}
+                        footer={null}
+                    >
+                        <iframe title="Polygot Pages Preschool" width="100%" height="450" src="https://www.youtube.com/watch?v=lfnQlU-FDSY"></iframe>
+                    </Modal>
+                </div>
+            </div>
+        );
+    };
 };
 
 export default Enrollment;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from './Views/Hero';
 import About from './About';
 import Feature from './Feature';
+import Enrollment from './Enrollment';
 
 
 
@@ -11,6 +12,7 @@ const Home = () => {
         <Hero/>
         <About />
         <Feature />
+        <Enrollment />
     </div>
   );
 };
